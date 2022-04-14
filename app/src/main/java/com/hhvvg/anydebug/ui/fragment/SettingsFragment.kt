d1fd43b5e098c59afeff4817b67510fccd4776d7
hhvvg <hvangchengxi@outlook.com>
2022-04-14 13:09:19 +0800

    Fix: Memory leak caused by receiver

--- app/src/main/java/com/hhvvg/anydebug/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/hhvvg/anydebug/ui/fragment/SettingsFragment.kt
@@ -35,6 +35,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
         EditControlReceiver.register(stateChangeReceiver, requireContext())
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        context?.unregisterReceiver(stateChangeReceiver)
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
         globalEditPreference?.setOnPreferenceChangeListener { _, newValue ->
